8e1cd250a424bc01260ce2b8dc8bfc9f0191bcc6
asadbekabduvoitov-nordra <asadbek.abduvoitov@nordra.com>
2024-06-08 18:30:21 +0500

    feat: navbar, sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import SideNav from "./components/sidenav";
+import Navbar from "./components/navbar";
+import SliderSecion from "./components/slider-section";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Navbar />
+			<SliderSecion />
+			<SideNav />
+		</div>
+	);
 };
 
 export default App;
